061e660e424e913cf3fb71c2f80126f703cd51c8
Marlom <falconsoft.3d@gmail.com>
2021-03-16 00:03:27 +0100

    sc

--- convert_minutes.js
+++ convert_minutes.js
@@ -5,3 +5,9 @@ function convert(min){
 console.log(convert(5))
 console.log(convert(3))
 console.log(convert(2))
+
+
+const convert2 = ( min ) => min * 60
+console.log(convert2(5))
+console.log(convert2(3))
+console.log(convert2(2))
